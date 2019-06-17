fb8e77777e0b8cfd06db02e30f740c2a2b6105db
Tyler Olsen <Tyler.Olsen1@gmail.com>
2019-06-17 14:15:51 -0600

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+const Index = () => {
+  return(
+    <div>
+      <h1>My name is Tyler</h1>
+      <h2>And Tucker is my wingman</h2>
+    </div>
+  )
+}
+
+export default Index
