f7e4448047d260903d5e51ec2302cfadf72e3f9a
Tyler Olsen <Tyler.Olsen1@gmail.com>
2019-06-28 15:58:58 -0600

    Set up component for contact form

--- src/components/form.js
+++ src/components/form.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 export class form extends Component {
   state = {
--- src/routes/contact.js
+++ src/routes/contact.js
@@ -1,10 +1,14 @@
 import React from "react"
 
 import Layout from "../components/layout"
+import Form from '../components/form'
 
 const Contact = () => {
   return(
     <Layout>
+      <Form>
+        
+      </Form>
     </Layout>
   )
 }
