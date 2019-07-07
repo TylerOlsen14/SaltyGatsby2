60d5e2fcf3775804a5fae20144143b3335cfcda1
Tyler Olsen <Tyler.Olsen1@gmail.com>
2019-07-07 17:15:57 -0600

    Form forwards to a success page

--- src/routes/home.js
+++ src/routes/home.js
@@ -19,21 +19,10 @@ const Home = () => (
       <Link to={paths.LaurelStreet}>Go to LaurelStreet</Link>
       <Link to={paths.EmmaStreet}>Go to EmmaStreet</Link>
     </div>
+    <div className="hidden">
+      <Link to={paths.success}>SUCCESS!!!</Link>
+    </div>
   </Layout>
 )
-
-// const Home = () => (
-//   <Layout>
-//     <h1>Hi people</h1>
-//     <p>Welcome to your new Gatsby site.</p>
-//     <p>Now go build something great.</p>
-//     <div className="HomeLink">
-//       <Link to={paths.about}>Go to About</Link>
-//       <Link to={paths.contact}>Contact US!!!</Link>
-//       <Link to={paths.LaurelStreet}>Go to LaurelStreet</Link>
-//       <Link to={paths.EmmaStreet}>Go to EmmaStreet</Link>
-//     </div>
-//   </Layout>
-// )
 
 export default Home
--- src/routes/success.js
+++ src/routes/success.js
@@ -1,13 +1,14 @@
 import React from 'react'
+
 import Layout from '../components/layout'
 
-const Success = () => {
+const Success = () => (
   <Layout>
     <div>
       <section>
         <div>
           <div>
-            <h1>Success/Thank You Page</h1>
+            <h1>Success/"Thank You" Page</h1>
             <h1>It's clobberin' time</h1>
           </div>
           <p>Thank you for contacting us!</p>
@@ -15,6 +16,6 @@ const Success = () => {
       </section>
     </div>
   </Layout>
-}
+)
 
 export default Success
