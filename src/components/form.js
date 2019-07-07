5922af74a93347762e9b0ef90b61060401be13d4
Tyler Olsen <Tyler.Olsen1@gmail.com>
2019-07-07 15:13:58 -0600

    added Form CSS & Form info

--- src/components/form.js
+++ src/components/form.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
+import "./form.css"
 
 export class form extends Component {
   state = {
     Name: "",
     Question: "",
+    Phonenumber: "",
   }
   handleInputChange = event => {
     const target = event.target
@@ -25,30 +27,61 @@ export class form extends Component {
           <h1>Let's talk together</h1>
           <h2>Phone number?</h2>
         </div>
-        <form action="">
-            <label for="Name">Your Name:
-              <input 
-                type="text"
-                class="form-control"
-                id="name"
-                name="name"
-                placeholder="Your Name:"
-                value={this.state.name}
-                onChange={this.handleInputChange}
-              />
-            </label>
-            <label for="Question">Your Question:
-              <input
-                type="text"
-                class="form-control"
-                id="question"
-                name="question"
-                placeholder="Your Question"
-                value={this.state.question}
-                onChange={this.handleInputChange}
-              />
-            </label>
-            <button type="submit">Submit</button>
+        <form
+          className="Form"
+          name="contact"
+          method="post"
+          action="#"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="bot-field" />
+          <input type="hidden" name="form-name" value="contact" />
+          <label for="Name">
+            Your Name:
+            <input
+              type="text"
+              class="form-control"
+              id="name"
+              name="name"
+              placeholder="Your Name:"
+              value={this.state.name}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <label>
+            Your Phone Number:
+            <input
+              type="tel"
+              class="form-control"
+              id="phone"
+              name="phone"
+              placeholder="Your Phone Number"
+              value={this.state.phonenumber}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <label for="Question">
+            Your Question:
+            <textarea
+              type="text"
+              class="form-control"
+              id="question"
+              name="question"
+              placeholder="Your Question"
+              value={this.state.question}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <ul className="actions">
+            <li>
+              <input type="submit" value="Send Message" className="special" />
+            </li>
+            <li>
+              <input type="reset" value="Clear" />
+            </li>
+          </ul>
+          {/* <button type="submit">Submit</button> */}
         </form>
       </div>
     )
