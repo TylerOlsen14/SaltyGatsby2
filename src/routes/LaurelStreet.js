dff51e98edb60ed5bb7a93a6db8a86d30dacd629
Tyler Olsen <Tyler.Olsen1@gmail.com>
2019-07-07 21:14:37 -0600

    added Carousel to Laurel Street w/ hard code

--- src/routes/LaurelStreet.js
+++ src/routes/LaurelStreet.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import {
-  Card,
-  CardTitle,
-  Carousel,
-  CarouselItem,
-  CarouselControl,
-  CarouselIndicators,
-  CarouselCaption
-} from 'reactstrap'
-import './propertyStyle.css'
-import Layout from '../components/layout'
+import React, { Component } from 'react';
+// import {
+//   Carousel,
+//   CarouselItem,
+//   CarouselControl,
+//   CarouselIndicators,
+//   CarouselCaption
+// } from 'reactstrap';
+import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import { Carousel } from 'react-responsive-carousel'
+import './propertyStyle.css';
+import Layout from '../components/layout';
 
 const items = [
   {
@@ -73,10 +74,10 @@ const items = [
   },
 ]
 
-class LaurelStreet extends React.Component {
+class LaurelStreet extends Component {
   constructor(props) {
-    super(props)
-    this.state = { activeIndex: 0};
+    super(props);
+    this.state = { activeIndex: 0 };
     this.next = this.next.bind(this);
     this.previous = this.previous.bind(this);
     this.goToIndex = this.goToIndex.bind(this);
@@ -87,63 +88,98 @@ class LaurelStreet extends React.Component {
   onExiting() {
     this.animating = true;
   }
+
   onExited() {
     this.animating = false;
   }
+
   next() {
     if (this.animating) return;
-    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 :this.state.activeIndex + 1;
+    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
     this.setState({ activeIndex: nextIndex });
   }
+
   previous() {
     if (this.animating) return;
     const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
     this.setState({ activeIndex: nextIndex });
   }
+
   goToIndex(newIndex) {
     if (this.animating) return;
-    this.setState({ activeIndex: newIndex })
+    this.setState({ activeIndex: newIndex });
   }
 
   render() {
     const { activeIndex } = this.state;
 
-    const slides = items.map((image) => {
-      return (
-        <CarouselItem
-        onExiting={this.onExiting}
-        onExited={this.onExited}
-        key={image.src}
-        >
-          <img className="CarouselImage" src={image.src} alt={image.altText} />
-          <CarouselCaption className="CarouselCaption" captionText={image.caption} captionHeader={image.caption} />
-        </CarouselItem>
-      )
-    })
+    // const slides = items.map((item) => {
+    //   return (
+    //     <CarouselItem
+    //       onExiting={this.onExiting}
+    //       onExited={this.onExited}
+    //       key={item.src}
+    //     >
+    //       <img src={item.src} alt={item.altText} />
+    //       <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+    //     </CarouselItem>
+    //   );
+    // });
 
     return (
-      <div>
-        <Layout>
-          <Card className="card">
-            <CardTitle className="title">
-              <p>
-                Laurel Street
-              </p>
-            </CardTitle>
-            <Carousel
-              activeIndex={activeIndex}
-              next={this.next}
-              prevous={this.previous}
-              className="Carousel carousel-fade"
-            >
-              <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
-              {slides}
-              <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
-              <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
-            </Carousel>
-          </Card>
-        </Layout>
-      </div>
+      <Layout>
+        <Carousel
+          activeIndex={activeIndex}
+          next={this.next}
+          previous={this.previous}
+        >
+          {/* <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+          {slides}
+          <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+          <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} /> */}
+          <div>
+            <img src="https://photos.zillowstatic.com/p_f/ISijensahjo2ph1000000000.jpg" alt="Items"/>
+            <p className="legend">Legend</p>
+          </div>
+          <div>
+            <img src="https://photos.zillowstatic.com/p_f/IS27azrx8388ph1000000000.jpg" alt="Items"/>
+            <p className="legend">Legend</p>
+          </div>
+          <div>
+            <img src="https://photos.zillowstatic.com/p_f/ISybza81oy2mph1000000000.jpg" alt="Items"/>
+            <p className="legend">Legend</p>
+          </div>
+          <div>
+            <img src="https://photos.zillowstatic.com/p_f/ISizum7ofimrph1000000000.jpg" alt="Items"/>
+            <p className="legend">Legend</p>
+          </div>
+          <div>
+            <img src="https://photos.zillowstatic.com/p_f/ISugomo43uxzph1000000000.jpg" alt="Items"/>
+            <p className="legend">Legend</p>
+          </div>
+          <div>
+            <img src="https://photos.zillowstatic.com/p_f/ISmama6yylp2qh1000000000.jpg" alt="Items"/>
+            <p className="legend">Legend</p>
+          </div>
+          <div>
+            <img src="https://photos.zillowstatic.com/p_f/IS6yhm5lq598qh1000000000.jpg" alt="Items"/>
+            <p className="legend">Legend</p>
+          </div>
+          <div>
+            <img src="https://photos.zillowstatic.com/p_f/ISqldy48ipsdqh1000000000.jpg" alt="Items"/>
+            <p className="legend">Legend</p>
+          </div>
+          <div>
+            <img src="https://photos.zillowstatic.com/p_f/ISa99a4v99cjqh1000000000.jpg" alt="Items"/>
+            <p className="legend">Legend</p>
+          </div>
+          <div>
+            <img src="https://photos.zillowstatic.com/p_f/ISuw4m3i1tvoqh1000000000.jpg" alt="Items"/>
+            <p className="legend">Legend</p>
+          </div>
+        </Carousel>
+      </Layout>
+
     );
   }
 }
--- src/routes/success.js
+++ src/routes/success.js
@@ -1,19 +1,19 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
+
+import { paths } from './'
 
 const Success = () => (
   <Layout>
     <div>
-      <section>
-        <div>
-          <div>
-            <h1>Success/"Thank You" Page</h1>
-            <h1>It's clobberin' time</h1>
-          </div>
-          <p>Thank you for contacting us!</p>
-        </div>
-      </section>
+      <div>
+        <h1>Success!</h1>
+        <h3>Your question was submited to our team.</h3>
+      </div>
+      <p>Thank you for contacting us!</p>
+      <Link to={paths.home}>HOME</Link>
     </div>
   </Layout>
 )
